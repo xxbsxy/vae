a260376040ea88455038b834cac342bfee68b455
陈桂东 <9542762+cgd-cgd@user.noreply.gitee.com>
2022-06-16 22:48:45 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@
 <script setup>
 
 import Header from './components/Header.vue'
+import Header from './components/Header.vue'
 import Aside from './components/Aside.vue'
 import Footer from './components/Footer.vue'
 </script>
